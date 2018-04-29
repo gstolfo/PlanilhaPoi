7340aff6ddd8e9f83014eb5e08f58c69f5a7dd8b
Guilherme Luiz Stolfo <guilhermeluizstolfo@MacBook-Pro-de-Guilherme-Luiz-Stolfo.local>
2018-04-29 11:12:06 -0300

    alteração

--- src/main/java/br/com/customizze/planilhaexcel/ReadExcel.java
+++ src/main/java/br/com/customizze/planilhaexcel/ReadExcel.java
@@ -3,17 +3,23 @@ package br.com.customizze.planilhaexcel;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -33,51 +39,38 @@ public class ReadExcel extends HttpServlet {
 			// Abrindo o arquivo e recuperando a planilha			
 			URL url = getClass().getResource(filePath);
 			File f = new File(url.getPath());
-		
 			FileInputStream file = new FileInputStream(f);
 			
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
-			XSSFSheet sheet = workbook.getSheetAt(0);
 			
-			List products = new ArrayList<>();
 			
-			Iterator rowIterator = sheet.rowIterator();
+			//Dados da Rede
+			XSSFSheet sheet = workbook.getSheetAt(2);	
+			Cell cell = null;
 			
-			/*while (rowIterator.hasNext()) {
-				Row row = (Row) rowIterator.next();
-				
-				// Descantando a primeira linha com o header
-				if(row.getRowNum() == 0){
-					continue;
-				}
-		
-				Iterator cellIterator = row.cellIterator();
-				
-				while (cellIterator.hasNext()) {
-					
-					Cell cell = (Cell) cellIterator.next();
+			//Update the value of cell
+			cell = sheet.getRow(1).getCell(8);
+			cell.setCellValue("Avaliação");
+			
+			
+			
+			//write out the XLSM
+			//FileOutputStream out = new FileOutputStream(f);
+			//workbook.write(out);
+			//out.close();
 
-					Product product = new Product();
-					
-					switch (cell.getColumnIndex()) {
-						case 0:
-							product.setId(((Double)cell.getNumericCellValue()).longValue());
-						break;
-						case 1:
-							product.setName(cell.getStringCellValue());
-						break;
-						case 2:
-							product.setPrice(cell.getNumericCellValue());
-						break;
-					}
-					products.add(product);
-				}
 			
-			}
+			//FileOutputStream os = new FileOutputStream(f); 
+			//workbook.write(os); 
 			
-			for (Product product : products) {
-				System.out.println(product.getId() + " – " + product.getName() + " – " + product.getPrice());
-			}*/
+			//FileOutputStream fileOut = new FileOutputStream(f);
+
+			//write this workbook to an Outputstream.
+			//workbook.write(fileOut);
+			//fileOut.flush();
+			//fileOut.close();
+			System.out.println("Writing on XLSX file Finished ...");
+
 			
 			file.close();
 			workbook.close();
